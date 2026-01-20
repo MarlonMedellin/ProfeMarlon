ba0d8d1acda28d066b936385a9b84a1033b22fcf
MarlonMedellin <mdav@outlook.com>
2026-01-19 23:41:52 -0500

    perf: optimize images for pagespeed insights scores

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
-import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: cloudflare({
-    mode: 'directory'
-  }),
+  output: 'static',
   integrations: [
     tailwind(),
     icon()
