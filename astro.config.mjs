88a156f69b4a88104755e7dcdbbb93080b4e8970
Profe Marlon Arcila <90739795+MarlonMedellin@users.noreply.github.com>
2026-01-19 21:59:02 -0500

    Add Cloudflare integration to Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
+import cloudflare from 'astro/integrations/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: cloudflare(),
   integrations: [
     tailwind(),
     icon()
